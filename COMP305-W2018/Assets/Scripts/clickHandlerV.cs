6e711e4e176924cd32a83574b964192dbc22da74
jibinsamreji <jibinsamreji@gmail.com>
2018-02-04 15:52:09 -0500

    Lab 02
    Lab 02 Completed. Successfully completed the assignment to demonstarte a small color changing scene.

---
+++ COMP305-W2018/Assets/Scripts/clickHandlerV.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickHandlerV : MonoBehaviour {
+
+    public GameObject human;
+    public Sprite firstImage;
+    public Sprite secondImage;
+    public Sprite thirdImage;
+
+
+
+    private SpriteRenderer spriteRend;
+
+    void Start()
+    {
+        // Initialization of component references
+
+        spriteRend = human.GetComponent<SpriteRenderer>();
+
+    }
+    // Executes when a mouse down event occurs on an object with this script.
+    void OnMouseDown()
+    {
+        if (spriteRend.sprite != thirdImage)
+        {
+            spriteRend.sprite = thirdImage;
+        }
+        else
+        {
+            spriteRend.sprite = thirdImage;
+        }
+
+
+    }
+}
